fd7f3aec2590900c31914af3e2e5b975fca3467b
git-savio1.domi <techsavvydominic3@gmail.com>
2023-06-11 10:58:17 +0300

    Updated schema.rb with database table definitions and foreign key constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_28_130218) do
+ActiveRecord::Schema.define(version: 2023_06_10_202437) do
 
-    create_table "categories", force: :cascade do |t|
-      t.string "category"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-    end
-  
-    create_table "todos", force: :cascade do |t|
-      t.string "name"
-      t.string "description"
-      t.integer "category_id", null: false
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.index ["category_id"], name: "index_todos_on_category_id"
-    end
-  
-    create_table "users", force: :cascade do |t|
-      t.string "name"
-      t.string "email"
-      t.integer "phone_number"
-      t.string "password"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-    end
-  
-    add_foreign_key "todos", "categories"
+  create_table "categories", force: :cascade do |t|
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_todos_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "phone_number"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "todos", "categories"
+end
